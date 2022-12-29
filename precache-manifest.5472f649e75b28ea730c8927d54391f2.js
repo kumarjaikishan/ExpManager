83d6d9acac8609b798d4fac944d09b05fd2b6879
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2022-12-29 09:37:56 +0530

    fgbg

---
+++ precache-manifest.5472f649e75b28ea730c8927d54391f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d04d8afedb01e20af62e",
+    "url": "/static/css/main.f0667451.chunk.css"
+  },
+  {
+    "revision": "d04d8afedb01e20af62e",
+    "url": "/static/js/main.d04d8afe.chunk.js"
+  },
+  {
+    "revision": "763c28fe76586f28f9da",
+    "url": "/static/js/1.763c28fe.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fc8f592ec638e0ec16696ccdafd114d1",
+    "url": "/static/media/background.fc8f592e.jpg"
+  },
+  {
+    "revision": "82ec443e7e87d6fa3a138095c0d9c57b",
+    "url": "/index.html"
+  }
+];
